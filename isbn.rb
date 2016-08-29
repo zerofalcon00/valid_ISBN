eae5744cc8d161242687b6b9d6225198dd94a3c6
Josh <mcnett88@gmail.com>
2016-08-29 14:34:06 -0400

    added test to convert string to array and to remove spaces and dashes

--- isbn.rb
+++ isbn.rb
@@ -1,3 +1,21 @@
-def valid_isbn?(num)
-	false
+def valid_isbn_length?(num)
+	if num.length == 10
+		true
+	else
+		false
+	end
+end
+
+def string_to_array(string)
+	string.split("")
+end
+
+def string_remove_space_or_dash(string)
+	if string.include? " "
+		string.delete " "
+	elsif string.include? "-"
+		string.delete "-"
+	else
+		string
+	end	
 end
--- isbn_test.rb
+++ isbn_test.rb
@@ -1,13 +1,40 @@
 require_relative "isbn.rb"
 require "minitest/autorun"
 
-class ValidIsbn < Minitest::Test
+class TestValidIsbn < Minitest::Test
 
 	def test_empty_string_returns_false()
-		assert_equal(false, valid_isbn?(""))
+		assert_equal(false, valid_isbn_length?(""))
 	end
 
-	# def test_non_empty_string_returns_true
-	# 	assert_equal(true, valid_isbn("0471958697"))
-	# end
+	def test_valid_10_digit_isbn_string_returns_true()
+		assert_equal(true, valid_isbn_length?("0471958697"))
+	end
+
+	def test_an_invalid_10_digit_isbn
+		skip("no") 
+		assert_equal(false, valid_isbn_length("0471958699"))
+	end
+end
+
+class TestConvertStringToArray < Minitest::Test
+
+	def test_empty_string_returns_empty_array
+		assert_equal([], string_to_array(""))
+	end
+
+	def test_string_returns_returns_array
+		assert_equal(["1","2","3","4","5"], string_to_array("12345"))
+	end
+end
+
+class TestToCheckForDashOrSpace < Minitest::Test
+	
+	def test_string_with_space_returns_string_with_no_space
+		assert_equal("12345", string_remove_space_or_dash("123 45"))
+	end
+
+	def test_string_with_dash_returns_string_with_no_dash
+		assert_equal("12345", string_remove_space_or_dash("123-45"))
+	end
 end
